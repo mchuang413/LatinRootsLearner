551e03f0a573603de9e09f112d01d307464b215f
Michael Chuang <85854593+mchuang413@users.noreply.github.com>
2024-11-01 19:16:30 -0700

    Update layout.js

--- app/app/layout.js
+++ app/app/layout.js
@@ -1,19 +1,18 @@
-import React from 'react';
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const Layout = ({ children }) => {
-  return (
-    <div>
-      <header>
-        <h1>Latin Roots Learner</h1>
-      </header>
-      <main>
-        {children}
-      </main>
-      <footer>
-        <p>&copy; 2023 Latin Roots Learner. All rights reserved.</p>
-      </footer>
-    </div>
-  );
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Rootified",
+  description: "",
 };
 
-export default Layout;
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+    );
+}
+
